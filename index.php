5b2beed6cc929904ed7dbc7a48c9101c12e2e952
kakakai <kaisakai15@gmail.com>
2019-11-27 21:53:01 +0700

    #master: try again

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
-<script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
-<script>paypal.Buttons().render('body');</script>
+<!-- <script src="https://www.paypal.com/sdk/js?client-id=sb"></script>
+<script>paypal.Buttons().render('body');</script> -->
 <form target="_new" method="post" action="https://boiling-bayou-19352.herokuapp.com/ipnListener/ipnlistener.php">
-  <input type="hidden" name="SomePayPalVar" value="SomeValue1"/>
-  <input type="hidden" name="SomeOtherPPVar" value="SomeValue2"/>
+  <!-- <input type="hidden" name="SomePayPalVar" value="SomeValue1"/>
+  <input type="hidden" name="SomeOtherPPVar" value="SomeValue2"/> -->
 
   <!-- code for other variables to be tested ... -->
 
