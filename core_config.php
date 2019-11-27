d8d8469a13023cb536a5fed83af854b27666ed1e
kakakai <kaisakai15@gmail.com>
2019-11-27 23:35:16 +0700

    #master: fixing

--- core_config.php
+++ core_config.php
@@ -17,8 +17,8 @@ $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 $system_mode = 'test'; // set 'test' for sandbox and 'live' for real payments.
 $paypal_seller = 'sb-xgis2645680@business.example.com'; //Your PayPal account email address
 
-$payment_return_success = 'https://demo.dopehacker.com/paypal_integration/payment_success.php'; //after payment, user will be redirected in this page, change with your own url
-$payment_return_cancel = 'https://demo.dopehacker.com/paypal_integration/payment_cancel.php'; //if payment cancelled, user will be redirected in this page, change with your own url
+$payment_return_success = 'SUCCESS'; //after payment, user will be redirected in this page, change with your own url
+$payment_return_cancel = 'CANCEL'; //if payment cancelled, user will be redirected in this page, change with your own url
 
 if ($system_mode == 'test') {$paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr'; $enable_sandbox = true; } else {$paypal_url = 'https://www.paypal.com/cgi-bin/webscr';}
 ?>
