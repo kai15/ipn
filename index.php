63a08cd10fd29f5f0c32880adc7d01a194012c58
kakakai <kaisakai15@gmail.com>
2019-11-27 20:20:52 +0700

    #master: echooo

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
 // the directory path of the certificate as shown below:
 // curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
 if ( !($res = curl_exec($ch)) ) {
-  error_log("Got " . curl_error($ch) . " when processing IPN data");
+  echo "Got " . curl_error($ch) . " when processing IPN data";
   curl_close($ch);
   exit;
 }
