c2b4082bbfc78fc41d7d4fb18bc7cfcfb7cb9dff
kakakai <kaisakai15@gmail.com>
2019-12-19 01:15:20 +0700

    #master: fix you v-120

--- ipn_listener1.0.php
+++ ipn_listener1.0.php
@@ -139,9 +139,7 @@ if (strcmp(($res), ("VERIFIED")) == 0) {
 	// }
 
 } else {
-	echo $key. " ";
-	echo $_POST. " ";
-	echo $res. " ALIAS JAJAL";
+	echo $res. $_POST['payment_status'];
 	// IPN invalid, log for manual investigation
 	// mysql_query("insert into log_dat(log_name, log_post, log_response, log_time) value('INVALID', '$req', '$res', now())");
 	// // emailCreditPayment('Sofian', 'raden.sofian.bahri@gmail.com', $req, json_encode($_POST), "INVALID", "", "");
